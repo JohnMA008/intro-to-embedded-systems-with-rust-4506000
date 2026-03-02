7cd88456a49fe53c031933461b07ab04578898f7
JohnMA008 <john.atkins3@defence.gov.au>
2026-03-02 01:20:54 +0000

    chapter 2 done

--- embedded/src/main.rs
+++ embedded/src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+use cortex_m::asm;
+use cortex_m::peripheral::syst::SystClkSource;
+use cortex_m::Peripherals;
+use cortex_m_rt::{entry, exception};
+use cortex_m_semihosting::hprintln;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+// CPU Frequency
+const CPU_FREQ: u32 = 12500000;
+
+#[entry]
+fn main() -> ! {
+    hprintln!("Running main.");
+
+    let peripherals: Peripherals = Peripherals::take().unwrap();
+    let mut systick = peripherals.SYST;
+    systick.enable_interrupt();
+    systick.set_clock_source(SystClkSource::Core);
+    systick.set_reload(CPU_FREQ);
+    systick.clear_current();
+    systick.enable_counter();
+
+    loop {
+        asm::wfi();
+    }
+}
+
+#[exception]
+fn SysTick() {
+    hprintln!("Woke up.");
 }
